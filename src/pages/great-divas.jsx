7ef209deba48255cbd01b9906fc52abe3afc15de
Selena Siri <selenasiri@gmail.com>
2020-03-11 19:31:20 -0400

    add onChange

--- src/pages/great-divas.jsx
+++ src/pages/great-divas.jsx
@@ -3,12 +3,29 @@ import Layout from '../components/layout/Layout'
 import SEO from '../components/SEO'
 
 const GreatDiva = () => {
+  const getArtist = event => {
+    console.log('ssssss')
+
+    const selectedArtistName = event.target.value
+    console.log(selectedArtistName)
+  }
+
   return (
     <Layout title="The Great Divas">
       <SEO title="Great Divas" />
 
-      <h1>Sorry,</h1>
-      <h3>We are currently working on "The Great Divas" at the moment!</h3>
+      <div>
+        <div id="artist"></div>
+      </div>
+
+      <h1>Search Artist</h1>
+      <select id="selectedArtist" onChange={getArtist}>
+        <option value="Maria Callas">Maria Callas</option>
+        <option value="Luciano Pavarotti">Luciano Pavarotti</option>
+        <option value="Joan Sutherland">Joan Sutherland</option>
+        <option value="Renata Tebaldi">Renata Tebaldi</option>
+        <option value="Montserrat Caballe">Montserrat Caballé</option>
+      </select>
     </Layout>
   )
 }
