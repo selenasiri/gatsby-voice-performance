56c8273d6facde276e9185c78ce695e7feb5d8b4
Selena Siri <selenasiri@gmail.com>
2020-03-11 18:39:59 -0400

    Selector on Registration

--- src/pages/registration.jsx
+++ src/pages/registration.jsx
@@ -60,10 +60,77 @@ const Registration = () => {
           <input type="text" name="streetName" id="streetName" />
 
           <br />
-          <br />
 
           <label htmlFor="city">City:</label>
           <input type="text" name="city" id="city" />
+
+          <div className="state">
+            <label for="state">State </label>
+            {/* <input type="text" name="state " id="state" title="state" /> */}
+            <select name="state" id="state">
+              <option value=""></option>
+              <option value="alabama">Alabama</option>
+              <option value="alaska">Alaska</option>
+              <option value="arizona">Arizona</option>
+              <option value="arkansas">Arkansas</option>
+              <option value="california">California</option>
+              <option value="colorado">Colorado</option>
+              <option value="connecticut">Connecticut</option>
+              <option value="deliware">Deliware</option>
+              <option value="florida">Florida</option>
+              <option value="georgia">Georgia</option>
+
+              <option value="hawaii">Hawaii</option>
+              <option value="idaho">Idaho</option>
+              <option value="illinois">Illinois</option>
+              <option value="indiana">Indiana</option>
+              <option value="iowa">Iowa</option>
+              <option value="kansas">Kansas</option>
+              <option value="kentucky">Kentucky</option>
+              <option value="louisiana">Louisiana</option>
+              <option value="maine">Maine</option>
+              <option value="maryland">Maryland</option>
+
+              <option value="massachusetts">Massachusetts</option>
+              <option value="michigan">Michigan</option>
+              <option value="minnesota">Minnesota</option>
+              <option value="mississippi">Mississippi</option>
+              <option value="missouri">Missouri</option>
+              <option value="montana">Montana</option>
+              <option value="nebraska">Nebraska</option>
+              <option value="nevada">Nevada</option>
+              <option value="newHampshire">New Hampshire</option>
+              <option value="newJersey">New Jersey</option>
+
+              <option value="newMexico">New Mexico</option>
+              <option value="newYork">New York</option>
+              <option value="northCarolina">North Carolina</option>
+              <option value="northDakota">North Dakota</option>
+              <option value="Ohio">Ohio</option>
+              <option value="Oklahoma">Oklahoma</option>
+              <option value="Oregon">Oregon</option>
+              <option value="Pennsylvania">Pennsylvania</option>
+              <option value="rhodeIsland">Rhode Island</option>
+              <option value="southCarolina">South Carolina</option>
+
+              <option value="southDakota">South Dakota</option>
+              <option value="tennessee">Tennessee</option>
+              <option value="texas">Texas</option>
+              <option value="utah">Utah</option>
+              <option value="vermont">Vermont</option>
+              <option value="virginia">Virginia</option>
+              <option value="washington">Washington</option>
+              <option value="westVirginia">West Virginia</option>
+              <option value="wisconsin">Wisconsin</option>
+              <option value="wyoming">Wyoming</option>
+            </select>
+          </div>
+          <br />
+
+          {/* Zip-code 
+
+
+          Telephone */}
         </fieldset>
         <br /> <br />
         <input className="submit-btn" type="submit" value="Validation Form" />
