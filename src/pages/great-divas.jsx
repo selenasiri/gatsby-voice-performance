b727c5de4e9bf7e475a2a83932cc74566aa4887f
Selena Siri <selenasiri@gmail.com>
2020-03-14 16:24:21 -0400

    great divas

--- src/pages/great-divas.jsx
+++ src/pages/great-divas.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../components/layout/Layout'
 import SEO from '../components/SEO'
+import mariaCallas from '../assets/images/maria-callas.jpeg'
 
 const GreatDiva = () => {
-  const getArtist = event => {
-    console.log('ssssss')
+  const [artstName, setArtstName] = useState('Maria Callas')
 
+  const getArtist = event => {
     const selectedArtistName = event.target.value
     console.log(selectedArtistName)
+    setArtstName(selectedArtistName)
   }
+
+  useEffect(() => {
+    console.log(artstName)
+    document.title = `${artstName} | Vocal Performance`
+  }, [artstName])
+
+  console.log('YYYYYY')
 
   return (
     <Layout title="The Great Divas">
       <SEO title="Great Divas" />
 
       <div>
-        <div id="artist"></div>
+        <div id="artist">
+          <h1>{artstName}</h1>
+          <img
+            className="mariaCallas"
+            src={mariaCallas}
+            alt="mariaCallas"
+          />{' '}
+          <h3>Followers: 176,760</h3>
+        </div>
       </div>
-
       <h1>Search Artist</h1>
       <select id="selectedArtist" onChange={getArtist}>
         <option value="Maria Callas">Maria Callas</option>
